bb8db067bc77563dbfb4d525a72432ff976b7ecb
Lucas Géhin <stagiairelucas@medialta.com>
2018-08-18 19:31:28 +0200

    create Play component and basic clues display

---
+++ src/components/Play.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <div v-for="question in questions.clues" :key="question">{{question}}</div>
+    </div>
+</template>
+
+<script>
+import json from '@/assets/data/question.json'
+export default{
+  data () {
+    return {
+      questions: json
+    }
+  }
+}
+</script>
