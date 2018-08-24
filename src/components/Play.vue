e675b342910a4cadca4281a61b821b9ad263c7ed
Lucas Géhin <stagiairelucas@medialta.com>
2018-08-24 17:55:35 +0200

    add a first set of data and randomize json

--- src/components/Play.vue
+++ src/components/Play.vue
@@ -6,18 +6,20 @@
         <router-link to="/">Quitter</router-link>
       </div>
       <div v-if="!gamePaused">
-        <span @click="setGamePaused(true)">set pause</span>
+        <span @click="setGamePaused(true)" class="pause">set pause</span>
       </div>
       <div v-if="timer !== false">
         <div v-if="currentQuestion === 0">La partie commence dans</div>
         <div v-else>Prochaine question dans</div>
         {{timer}}
       </div>
-      <div v-if="clue">{{clue}}</div>
+      <div v-if="clue" class="clue">
+        <img :src="'static/clue/' + clue + '.png'">
+      </div>
       </div>
       <div v-else>
         Réponses
-        <div v-for="question in questions">
+        <div v-for="question in questions" :key="question.answer">
           {{question.answer}}
         </div>
       </div>
@@ -25,12 +27,18 @@
 </template>
 
 <script>
-import questions from '@/assets/data/question.json'
+import json from '@/assets/data/question.json'
 
+var questions = json
+questions = questions.sort(function () {
+  return 0.5 - Math.random()
+})
+
+var maxQuestion = 5
+var gameQuestions = questions.slice(0, maxQuestion)
 var timer = false
-var changeTime = 3000
+var changeTime = 2000
 var currentQuestion = 0
-var maxQuestion = 3
 var currrentClue = 0
 var maxClue = 5
 
@@ -39,7 +47,7 @@ export default {
     return {
       gameRunning: false,
       gamePaused: false,
-      questions: questions,
+      questions: gameQuestions,
       currentQuestion: currentQuestion,
       currrentClue: currrentClue,
       timer: timer,
@@ -56,7 +64,11 @@ export default {
     },
     runTimer () {
       return new Promise((resolve, reject) => {
-        this.timer = 5
+        var countdown = 10
+        if (this.currentQuestion === 0) {
+          countdown = 5
+        }
+        this.timer = countdown
         const timerInterval = setInterval(function () {
           if (this.timer === 0) {
             clearInterval(timerInterval)
@@ -114,5 +126,26 @@ export default {
     left: 0;
     right: 0;
     background-color: rgba(0, 0, 0, 0.5);
+  }
+
+  .pause {
+    position: fixed;
+    top: 10px;
+    right: 10px;
+  }
+
+  .clue {
+    background-color: rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+    position: fixed;
+    top: 10%;
+    bottom: 10%;
+    left: 10%;
+    right: 10%;
+  }
+  .clue img {
+    position: absolute;
+    top: 50%;
+    transform: translate(-50%, -50%);
   }
 </style>
